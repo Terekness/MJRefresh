6c58fa730de6eb02d1143588bed95ddbdddf61c6
Terekness <tareksej@hotmail.com>
2017-02-14 16:20:41 +0100

    Update MJRefreshBackStateFooter.h

--- MJRefresh/Custom/Footer/Back/MJRefreshBackStateFooter.h
+++ MJRefresh/Custom/Footer/Back/MJRefreshBackStateFooter.h
@@ -6,7 +6,16 @@
 //  Copyright © 2015年 小码哥. All rights reserved.
 //
 
-#import "MJRefreshBackFooter.h"
-
-@interface MJRefreshBackStateFooter : MJRefreshBackFooter
-@end
+ #import "MJRefreshBackFooter.h"
+  
+  @interface MJRefreshBackStateFooter : MJRefreshBackFooter
+ -/** 文字距离圈圈、箭头的距离 */
+ -@property (assign, nonatomic) CGFloat labelLeftInset;
+ -/** 显示刷新状态的label */
+ -@property (weak, nonatomic, readonly) UILabel *stateLabel;
+ -/** 设置state状态下的文字 */
+ -- (void)setTitle:(NSString *)title forState:(MJRefreshState)state;
+ -
+ -/** 获取state状态下的title */
+ -- (NSString *)titleForState:(MJRefreshState)state;
+  @end
